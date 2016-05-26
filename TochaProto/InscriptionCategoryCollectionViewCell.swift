e0195edff53ea0a7c8c21a76187bb4b02ed8178d
MathisDetourbet <mathis.detourbet@gmail.com>
2016-05-26 03:08:15 +0200

    inscription 2 (100%), 3 (90%), 4 (90%)

---
+++ TochaProto/InscriptionCategoryCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  InscriptionCategoryCollectionViewCell.swift
+//  DochaProto
+//
+//  Created by Mathis D on 25/05/2016.
+//  Copyright © 2016 LaTV. All rights reserved.
+//
+
+import Foundation
